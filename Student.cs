b3540aed87a0a6eb67e2d8962548c9e787338b87
Nhan Khánh Đình <dinhnkgcs220022@fpt.edu.vn>
2025-02-28 07:15:49 +0700

    Thêm mới 2 model Student và Tutor (đang chỉnh sửa)

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolSystem.Models
+{
+    public class Student : AppUser
+    {
+        public required string StudentId { get; set; } // Mã sinh viên
+        public int? GradeLevel { get; set; } // Sinh viên năm bao nhiêu (vd : 1, 2, 3, 4, 5)
+        public string? Major { get; set; } // Chuyên ngành
+        public string? Bio { get; set; } // Giới thiệu bản thân (có thể để trống)
+    }
+}
---
+++ Tutor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolSystem.Models
+{
+    public class Tutor : AppUser
+    {
+        public required string TutorId { get; set; } // Mã giáo viên
+        public string? SubjectSpecialization { get; set; } // Chuyên ngành giảng dạy
+        public string? Qualification { get; set; } // Bằng cấp
+        public string? Bio { get; set; } // Giới thiệu bản thân (có thể để trống)
+    }
+}
