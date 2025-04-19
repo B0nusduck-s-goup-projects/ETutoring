259cfa961d89f323531be192368144d3fb7b2367
B0nusduck <hhnam22310@gmail.com>
2025-04-19 16:07:10 +0700

    fix querry bug in add group view

--- SchoolSystem/ViewModels/GroupCreateVM.cs
+++ SchoolSystem/ViewModels/GroupCreateVM.cs
@@ -5,8 +5,8 @@ namespace SchoolSystem.ViewModels
 {
     public class GroupCreateVM
     {
-        public List<AppUser> Teachers { get; set; } = null!;
-        public List<AppUser> Students { get; set; } = null!;
+        public List<AppUser>? Teachers { get; set; } = null!;
+        public List<AppUser>? Students { get; set; } = null!;
         public List<string>? exception { get; set; }
 
     }
